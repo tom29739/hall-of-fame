cc04a9572c70c72b9049113ddca95d6f661f8b22
tom29793 <tom-29@live.co.uk>
2016-03-16 21:10:50 +0000

    Corrected concatenation.

--- index2.php
+++ index2.php
@@ -4,7 +4,7 @@
 $wiki = $_POST['wikicode'];
 $wikic =  $_POST['wikiarticle'];
 //$wikia = file_get_contents("https://".urlencode($wiki)".wikipedia.org/w/index.php?title=".urlencode($wikic)"&action=raw");
-$wikia = "https://" . urlencode($wiki) . ".wikipedia.org/w/index.php?title=" . urlencode($wikic) . "&action=raw";
+$wikia = 'https://' . urlencode($wiki) . '.wikipedia.org/w/index.php?title=' . urlencode($wikic) . '&action=raw';
 $br = '<br>';
 echo $wiki;
 echo $br;
