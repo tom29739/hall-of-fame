f3b62f2fac075741e6d9ac2d79c04c0d27b354b3
tom29793 <tom-29@live.co.uk>
2016-03-16 21:17:36 +0000

    Getting file contents now.

--- index2.php
+++ index2.php
@@ -3,14 +3,16 @@
 <?php
 $wiki = $_POST['wikicode'];
 $wikic =  $_POST['wikiarticle'];
-//$wikia = file_get_contents("https://".urlencode($wiki)".wikipedia.org/w/index.php?title=".urlencode($wikic)"&action=raw");
 $wikia = 'https://' . urlencode($wiki) . '.wikipedia.org/w/index.php?title=' . urlencode($wikic) . '&action=raw';
+$wikiar = file_get_contents('https://' . urlencode($wiki) . '.wikipedia.org/w/index.php?title=' . urlencode($wikic) . '&action=raw');
 $br = '<br>';
 echo $wiki;
 echo $br;
 echo $wikic;
 echo $br;
 echo $wikia;
+echo $br;
+echo $wikiar;
 // echo $wikia;
 // $wikicode =
 // $wikiarticle =
