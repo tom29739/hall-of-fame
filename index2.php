43dd82aced37212e5bfe066f3439c948c29778bf
tom29739 <tom29739@users.noreply.github.com>
2016-03-26 15:47:04 +0000

    Added new dependencies.

--- index2.php
+++ index2.php
@@ -4,7 +4,7 @@
 $wiki = $_POST['wikicode'];
 $wikic =  $_POST['wikiarticle'];
 $wikia = 'https://' . urlencode($wiki) . '.wikipedia.org/w/index.php?title=' . urlencode($wikic) . '&action=raw';
-$wikiar = file_get_contents('https://' . urlencode($wiki) . '.wikipedia.org/w/index.php?title=' . urlencode($wikic) . '&action=raw');
+$wikiar = file_get_contents('https://' . urlencode($wiki) . '.wikipedia.org/w/index.php?title=' . urlencode($wikic) . '&action=render');
 $br = '<br>';
 echo $wiki;
 echo $br;
